c91336c8ab597b320bd160fe4e5c283ea6642a53
André Gustavo Silveira <andregustavocrtb@gmail.com>
2025-01-16 21:24:34 -0300

    fix-final

--- desafio_cht1.py
+++ desafio_cht1.py
@@ -1,3 +1,14 @@
+# 1. Teste de Login com Credenciais Incorretas - Web
+# Dado:
+# 	• O usuário possui um formulário de login com campos para e-mail e senha.
+# 	• O sistema está funcionando e esperando as credenciais do usuário.
+# Quando:
+# 	• O usuário tenta fazer login com um e-mail e senha incorretos.
+# 	E-mail : teste1@teste.com
+# 	Senha: Teste
+# Então:
+# O sistema não permite o login e exibe uma mensagem de erro dizendo "Authentication failed", garantindo que apenas credenciais corretas possam acessar a conta.
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,7 +19,7 @@ browser = init_setup()
 
 try:
 
-    def case_1(): #Dado que o usuário tenta logar com credenciais incorretas - web
+    def case_1():
         try:
             WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "email")))
             login("teste1@teste.com","Teste",browser)
@@ -16,9 +27,9 @@ try:
             error_message = error_message_login(browser)
             assert "Authentication failed" in error_message.text.strip()
             print("Case 1 executed successfully")
-            
+
         except Exception as e:
-            print(f"Erro durante o Caso 1: {e}")
+            print(f"Error during Case 1: {e}")
 
     case_1()
 
--- desafio_cht2.py
+++ desafio_cht2.py
@@ -1,3 +1,15 @@
+# 2. Teste de Login com Credenciais Incorretas - Mobile
+# Dado:
+# 	• O usuário possui um formulário de login com campos para e-mail e senha.
+# 	• O sistema está funcionando e esperando as credenciais do usuário, no mobile: Iphone X.
+# Quando:
+# 	• O usuário tenta fazer login com um e-mail e senha incorretos.
+# 	E-mail: teste1@teste.com
+# 	Senha: Teste
+# Então:
+# 	• O sistema não permite o login e exibe uma mensagem de erro dizendo "Authentication failed", garantindo que apenas credenciais corretas possam acessar a conta.
+
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,16 +20,20 @@ browser = init_setup(mobile=True)
 
 try:
 
-    def case_2(): #Dado que o usuário tenta logar com credenciais incorretas - mobile
-        email_label = browser.find_element(By.ID, "email")
-        browser.execute_script("arguments[0].scrollIntoView({block: 'center'})", email_label)
-        login("teste1@teste.com","Teste",browser)
+    def case_2():
+        try:
+            email_label = browser.find_element(By.ID, "email")
+            browser.execute_script("arguments[0].scrollIntoView({block: 'center'})", email_label)
+            login("teste1@teste.com","Teste",browser)
 
-        error_message = error_message_login(browser)
-        assert "Authentication failed" in error_message.text.strip()
+            error_message = error_message_login(browser)
+            assert "Authentication failed" in error_message.text.strip()
+            print("Case 2 executed successfully.")
+
+        except Exception as e:
+            print(f"Error during Case 1: {e}")
 
     case_2()
-    print("Case 2 executed successfully.")
 
 finally:
     time.sleep(2)
